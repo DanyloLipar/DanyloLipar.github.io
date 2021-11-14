0e95509e288b0e8d5a25e92b499decab7b542073
DanyloLipar <danilo.lipar2016@gmail.com>
2021-11-14 20:41:44 +0200

    mycv

--- js/list.js
+++ js/list.js
@@ -1,18 +1,15 @@
 const slide = document.querySelector('.about-me');
-let text = document.createElement('p');
-let but = document.createElement('button')
+const text = document.createElement('p');
 
 
 slide.addEventListener('click', () => {
-    slide.classList.add('active');
     text.className = 'open-text';
-    text.innerText = `I am 20 yers old and I am a student. Besides I am hardworking and responsible person who likes improving my own skills. Except programming I love travelling and sports. My favorite kind of sport is football.`
-    but.className = 'cancel-but'
-    but.innerHTML = `<img src='/photo/close-btn.png' alt='btn-close'>`
+    if (text.innerText === "") {
+        text.style.display = 'initial';
+        text.innerText = `I am 20 yers old and I am a student. Besides I am hardworking and responsible person who likes improving my own skills. Except programming I love travelling and sports. My favorite kind of sport is football.`;
+    } else {
+        text.innerText = "";
+        text.style.display = "none";
+    }
     slide.append(text);
-    slide.append(but);
-})
-
-slide.addEventListener('dblclick', () => {
-    slide.removeChild(text);
 })
